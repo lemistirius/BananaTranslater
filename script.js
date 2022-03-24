9c0cd739c1483aa2da2d220a4553d158382107e9
lemistirius <lamster198@gmail.com>
2022-03-24 22:45:00 +0530

    Added input and output functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-var btnTranslate = document.querySelector("btn-translate")
+const btnTranslate = document.querySelector("#btn-translate")
+const getText = document.querySelector("#texttocon")
+const bananatext = document.querySelector("#bananaText")
+
+
+btnTranslate.addEventListener('click', function(){
+   const banana =  getText.value
+   console.log(banana)
+
+ //  async function fetchText() {
+ //   let response = await fetch(`/https://api.agify.io?name=bitch`);
+  //  let data = await response.json();
+  //  console.log(data);
+  fetch(`https://api.genderize.io/?name=${banana}`)
+  .then(res =>{
+      
+      res.json().then((data) =>{
+          console.log(data.gender)
+        const gen = data.gender
+          console.log(data.gender)
+          bananatext.textContent = gen
+      })
+      
+     
+  })
+
+
+
+
+
+})
+
