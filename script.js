2434456082474fa3c5c8eb3fb6585d58552456a1
lemistirius <lamster198@gmail.com>
2022-03-25 19:09:07 +0530

    Added Api and styles

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 const btnTranslate = document.querySelector("#btn-translate")
 const getText = document.querySelector("#texttocon")
 const bananatext = document.querySelector("#bananaText")
-
-
 btnTranslate.addEventListener('click', function(){
    const banana =  getText.value
-   console.log(banana)
-
- //  async function fetchText() {
- //   let response = await fetch(`/https://api.agify.io?name=bitch`);
-  //  let data = await response.json();
-  //  console.log(data);
-  fetch(`https://api.genderize.io/?name=${banana}`)
+  fetch(`https://api.funtranslations.com/translate/minion.json?text=${banana}`)
   .then(res =>{
-      
       res.json().then((data) =>{
-          console.log(data.gender)
-        const gen = data.gender
-          console.log(data.gender)
-          bananatext.textContent = gen
+        const trans = data.contents.translated
+        bananatext.textContent = trans
       })
-      
-     
   })
-
-
-
-
-
 })
 
